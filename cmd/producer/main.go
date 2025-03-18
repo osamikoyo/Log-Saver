816f794f7e69bfdea9b3eef5e01767157b79969d
osamikoyo <v.pylayev@mail.ru>
2025-03-18 22:33:22 +0500

    producer main.go

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"github.com/koyo-os/log-saver/internal/config"
+	"github.com/koyo-os/log-saver/internal/producer"
+	"github.com/koyo-os/log-saver/internal/recorder"
+	"github.com/koyo-os/log-saver/pkg/logger"
+)
+
+func main() {
+	logger := logger.Init()
+
+	cfg, err := config.InitPC()
+	if err != nil{
+		logger.Error(err.Error())
+		return
+	}
+
+	var ch chan []byte
+
+	producer, err := producer.Init(cfg, logger, ch)
+	if err != nil{
+		logger.Error(err.Error())
+		return
+	}
+
+	recorder := recorder.Init(cfg, logger, ch)
+	go producer.Listen()
+	recorder.Run()
+}
