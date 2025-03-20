0e859a406a18a8efb6026c7455392eeb58ebd8d7
osamikoyo <v.pylayev@mail.ru>
2025-03-20 06:00:01 +0500

    add delete saver func

--- pkg/saver/delete.go
+++ pkg/saver/delete.go
@@ -1,6 +1,9 @@
 package saver
 
-import "github.com/elastic/go-elasticsearch/v8/esapi"
+import (
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"go.uber.org/zap/zapcore"
+)
 
 func (s *Saver) Delete(id string) error {
 	req := esapi.DeleteRequest{
@@ -8,5 +11,19 @@ func (s *Saver) Delete(id string) error {
 		DocumentID: id,
 	}
 
-	
+	res, err := req.Do(s.ctx, s.client)
+	if err != nil{
+		 s.logger.Error("cant do req", zapcore.Field{
+			Key: "err",
+			String: err.Error(),
+		 })
+		 return err
+	}
+	defer res.Body.Close()
+
+	s.logger.Info("success request", zapcore.Field{
+		Key: "status",
+		Integer: int64(res.StatusCode),
+	})
+
 }
